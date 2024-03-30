39f751b02e156c4c5adb464eebeb2a000003fd44
jersonjabat <jersonjabatstudent@gmail.com>
2024-03-30 19:59:09 +0800

    update

--- config/puma.rb
+++ config/puma.rb
@@ -31,5 +31,10 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+preload_app!
+
+workers ENV.fetch("WEB_CONCURRENCY") {2}
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
